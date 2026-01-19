e49915975bd02ff70b8d146ac74e91b2c982899e
cheibe4 <medb02668@gmail.com>
2026-01-19 11:33:06 +0000

    Initial Spring Security setup with Basic Auth and User entity

---
+++ src/main/java/com/pointage/sista/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.pointage.sista.security;
+
+import com.pointage.sista.entity.User;
+import com.pointage.sista.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(()->
+                                new UsernameNotFoundException("User Not Found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+
+    }
+}
